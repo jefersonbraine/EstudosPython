2e61658fcfb3e44ab9542860c87d5df1e2438ae4
Jeferson <jefeleal363@gmail.com>
2025-03-05 18:58:52 -0300

    feat: modules 1 to 3

---
+++ Basico/TipoVariavel2.py
@@ -0,0 +1,9 @@
+codigo = 105
+salario = 1500.00
+nome = 'Jose Santana'
+situação = True
+
+print("Código: %d "% codigo)
+print("Salário: %.2f "% salario)
+print("Nome: %s "% nome)
+print("Situação: %r "% situação)
