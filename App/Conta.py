80a8516a611165e33f9f44aa56ca72b925518cd0
Jeferson <jefeleal363@gmail.com>
2025-03-06 21:07:58 -0300

    finished app

--- App/Conta.py
+++ App/Conta.py
@@ -1,5 +1,28 @@
 class Conta:
     def __init__(self, titular, numero, saldo):
-        self.saldo=0
-        self.numero=numero
-        self.titular=titular
+        self._saldo=0
+        self._numero=numero
+        self._titular=titular
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    def saldo(self, saldo):
+        if (saldo<0):
+            print("O saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
+
+    def saque(self, valor):
+        if (self.saldo>=valor):
+            self.saldo-=valor
+            print("Saque realizado com sucesso")
+        else:
+            print("Saldo insuficiente")
+
+    def deposita(self, valor):
+        self.saldo+=valor
+
+    def extrato(self):
+        print("Cliente: ", self.titular, " Saldo Atual: ", self._saldo)
--- App/Main.py
+++ App/Main.py
@@ -1,15 +1,22 @@
 class Main:
     pass
-
-print("Testando o projeto")
+#
+# print("Testando o projeto")
 
 from Cliente import Cliente
 
 from Conta import Conta
 
-c1 = Cliente("João", "11444-2222")
-conta=Conta(c1.nome, 6565, 0)
+c1 = Cliente("João", "114444-2222")
+conta=Conta(c1.get_nome(), 1222)
+
+# conta=Conta(c1.get_nome, 6565, 0)
+
+
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
 
 # print(c1)
 # print(c1.nome, " e ", c1.telefone)
-print(conta.titular, " Numero: ", conta.numero, " Seu Saldo: ", conta.saldo)
+# print(conta.titular, " Numero: ", conta.numero, " Seu Saldo: ", conta.saldo)
