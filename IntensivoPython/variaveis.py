6ebbf4c9082876462bdac4b9cfce96c30297f882
Jeferson <jefeleal363@gmail.com>
2025-03-14 10:45:20 -0300

    feat: intensivo em Python

---
+++ IntensivoPython/variaveis.py
@@ -0,0 +1,30 @@
+# nome = 'Seu nome'
+# print(nome)
+
+# nome_usuario = 'Seu nome de novo'
+# print(nome_usuario)
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Fabio', 47
+estado = True
+
+# Função type()
+
+print(type(media))
+print(type(n2))
+print(type(nome))
+print(type(estado))
+print(type(1+2j))
+
+# Função isinstance()
+a = 10
+b = 'Sol'
+print(isinstance(a, (int, float)))
+print(isinstance(b, str))
+
+a = 40
+c = 3
+r = a * c
+
+print (r)
