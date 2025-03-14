256fa31e52a9224b11bf79ea06e62c0f1a3c659d
Jeferson <jefeleal363@gmail.com>
2025-03-14 11:28:40 -0300

    feat: operadores logicos

---
+++ IntensivoPython/operadores_logicos.py
@@ -0,0 +1,29 @@
+
+# *AND
+
+# idade = 15
+# altura = 1.78
+
+# resultado = (idade >= 18) and (altura >= 1.70)
+
+# msg = 'Pode participar no evento? ' + str(resultado)
+
+# print(msg)
+
+
+# *Programar de disparo de alarme = OR
+# porta = 'a'
+# janela = 'f'
+
+# alarme = (porta == 'a') or (janela == 'a')
+
+# msg = 'Alarme diparado? ' + str(alarme)
+
+# print(msg)
+
+# *NOT
+tem_dinheiro = False
+tem_dinheiro = not tem_dinheiro
+
+msg = 'Tem dinheiro? ' + str(tem_dinheiro)
+print(msg)
